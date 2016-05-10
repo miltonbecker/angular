7bba8fb135f0fc63cdcbcef4c484be33d612ad01
Milton Becker Junior <miltonbecker@gmail.com>
2016-05-10 00:00:08 -0300

    Tabs and image gallery! Also, now using bootstrap classes!

--- app.js
+++ app.js
@@ -5,20 +5,49 @@
     this.products = gems;
   });
 
+  app.controller('PanelController', function(){
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
+
+  app.controller('GalleryController', function(){
+    this.current = 0;
+    this.setCurrent = function(index){
+      this.current = index || 0;
+    };
+  });
+
   var gems = [
     {
       name: 'Dodecahedron',
-      price: 2.95,
+      price: 2,
       description: 'Some description here',
       canPurchase: true,
-      soldOut: false
+      soldOut: false,
+      images: [
+        "images/gem-01.gif",
+        "images/gem-03.gif",
+        "images/gem-04.gif",
+      ]
     },
     {
       name: 'Pentagonal Gem',
       price: 5.95,
       description: 'Some description here',
       canPurchase: true,
-      soldOut: false
+      soldOut: false,
+      images: [
+        "images/gem-06.gif",
+        "images/gem-07.gif",
+        "images/gem-09.gif",
+      ]
     }
   ];
 })();
